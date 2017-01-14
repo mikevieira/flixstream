37490172d4397ead081b0c2211674ca7cff0984f
Mike Vieira <mikevieira@hotmail.com>
2017-01-14 22:27:04 +0000

    A few bug fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # DO fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = ture
+  config.assets.compile = true 
 
   # Generate digests for assets URLs.
   config.assets.digest = true
